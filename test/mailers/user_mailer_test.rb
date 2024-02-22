e5050e1de9ca0934b560bfccd065e919159a8616
iYoQ <iyoqpwnz@gmail.com>
2024-02-23 02:25:56 +0300

    mailer tests

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,4 +2,31 @@ require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
 
+  setup do
+    @users_number = (1..5)
+  end
+
+  test 'sending_daily_digest' do
+    @users_number.map{ |i| User.create(email: "#{i}@mail.com",
+                                password: '123456',
+                                send_news: User::SEND_NEWS_VALUES[:daily]) }
+
+    users_count = User.where(send_news: User::SEND_NEWS_VALUES[:daily]).count
+
+    User.daily_sending
+
+    assert_equal(users_count, ActionMailer::Base.deliveries.length)
+  end
+
+  test 'sending_weekly_digest' do
+    @users_number.map{ |i| User.create(email: "#{i}@mail.com",
+                                password: '123456',
+                                send_news: User::SEND_NEWS_VALUES[:weekly]) }
+
+    users_count = User.where(send_news: User::SEND_NEWS_VALUES[:weekly]).count
+
+    User.weekly_sending
+
+    assert_equal(users_count, ActionMailer::Base.deliveries.length)
+  end
 end
